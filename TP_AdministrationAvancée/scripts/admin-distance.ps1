75654972b2914aca5dc9c9b7f2fdb600c686bcdd
ValentinFoure <valentinfoure.pro@gmail.com>
2025-05-15 14:16:16 +0200

    Delete TP_AdministrationAvancée directory

--- TP_AdministrationAvancée/scripts/admin-distance.ps1
+++
@@ -1,5 +0,0 @@
-# Sécurité : demande des identifiants
-$cred = Get-Credential
-
-# Adresse IP ou nom DNS de la machine distante
-$remoteComputer = "PC-Valentin"
--- TP_AdministrationAvancée/scripts/taches-planifiees.ps1
+++
@@ -1,5 +0,0 @@
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File C:\Scripts\sauvegarde.ps1"
-$trigger = New-ScheduledTaskTrigger -Daily -At 2am
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
-
-Register-ScheduledTask -TaskName "SauvegardeDossier" -Action $action -Trigger $trigger -Principal $principal
